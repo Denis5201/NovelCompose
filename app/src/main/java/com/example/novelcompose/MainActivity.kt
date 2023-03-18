c5ecebce7af0764d482c0a4e286c6650f1a16db4
Denis5201 <builukden@yandex.ru>
2023-03-18 15:19:11 +0700

    Init

---
+++ app/src/main/java/com/example/novelcompose/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.novelcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.example.novelcompose.ui.theme.NovelComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NovelComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/novelcompose/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.example.novelcompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val darkGreen = Color(0xFF275168)
+val lightBlue = Color(0xFF73AAC7)
+val grey = Color(0xFFE8E8E8)
